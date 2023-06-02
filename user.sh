da75d3fa9c508b7a88bd50b71fcad5de27159714
sivarao <siva.nani4@gmail.com>
2023-06-02 13:24:59 +0530

    user changes

---
+++ user.sh
@@ -0,0 +1,16 @@
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
+cd /app 
+unzip /tmp/user.zip
+npm install 
+cp /home/centos/roboshopProject/user.service /etc/systemd/system/user.service
+systemctl daemon-reload
+systemctl enable user 
+systemctl start user
+
+cp /home/centos/roboshopProject/mongo.repo /etc/yum.repos.d/mongo.repo
+yum install mongodb-org-shell -y
+mongo --host mongodb.devopsr72.online </app/schema/user.js
