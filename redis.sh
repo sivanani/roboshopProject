c73cd100254b15dd1ecf95419085d80d929f2983
sivarao <siva.nani4@gmail.com>
2023-06-02 12:45:19 +0530

    redis message

---
+++ redis.sh
@@ -0,0 +1,9 @@
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+dnf module enable redis:remi-6.2 -y
+yum install redis -y 
+
+sed -i -e 's|127.0.0.1|0.0.0.0|' etc/redis.conf
+
+systemctl enable redis 
+systemctl start redis 
